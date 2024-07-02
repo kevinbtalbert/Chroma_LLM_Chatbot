9b6289318e1afba25b710a57f3c4376ed158bbb1
Kevin Talbert <ktalbert@cloudera.com>
2024-07-02 13:37:56 -0700

    updated application

--- llm-app.py
+++ llm-app.py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 import chromadb
 from chromadb.utils import embedding_functions
-from datetime import time
+import time
 
 def read_config(file_path):
     config = configparser.ConfigParser()
@@ -23,6 +23,8 @@ EMBEDDING_MODEL_NAME = config['EMBEDDING_MODEL_NAME']
 LLM_MODEL_NAME = config['LLM_MODEL_NAME']
 COLLECTION_NAME = config['COLLECTION_NAME']
 CHROMA_DATA_FOLDER = config['CHROMA_DATA_FOLDER']
+HOST = config['HOST']
+APP_PORT = int(config['APP_PORT'])
 
 # Connect to local Chroma data
 chroma_client = chromadb.PersistentClient(path=CHROMA_DATA_FOLDER)
@@ -60,11 +62,11 @@ tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(LLM_MODEL_NAME, config=bnb_config)
 
 # Define the inference function
-def generate(prompt, max_length=100, temperature=0.7):
+def generate_response(prompt, max_new_tokens=100, temperature=0.7):
     inputs = tokenizer(prompt, return_tensors="pt")
     output = model.generate(
         inputs["input_ids"],
-        max_length=max_length,
+        max_new_tokens=max_new_tokens,
         temperature=temperature,
         do_sample=True
     )
@@ -92,18 +94,17 @@ def main():
     )
 
     print("Launching gradio app")
-    demo.launch(share=True, enable_queue=True, show_error=True, server_name=config['HOST'], server_port=int(config['APP_PORT']))
+    demo.launch(share=True, show_error=True, server_name=HOST, server_port=APP_PORT)
     print("Gradio app ready")
 
 def get_responses(message, history, model, temperature, token_count, vector_db):
-    if model == "Local Mistral 7B":
-        if vector_db == "Chroma":
-            context_chunk, metadata = get_nearest_chunk_from_chroma_vectordb(collection, message)
-            response = generate(message, context_chunk, temperature, token_count)
-            response = f"{response}\n\nMetadata: {metadata}"
-            for i in range(len(response)):
-                time.sleep(0.02)
-                yield response[:i+1]
+    if model == "Local Mistral 7B" and vector_db == "Chroma":
+        context_chunk, metadata = get_nearest_chunk_from_chroma_vectordb(collection, message)
+        response = generate_response(f"{message} {context_chunk}", max_new_tokens=int(token_count), temperature=float(temperature))
+        response = f"{response}\n\nMetadata: {metadata}"
+        for i in range(len(response)):
+            time.sleep(0.02)
+            yield response[:i+1]
 
 def get_nearest_chunk_from_chroma_vectordb(collection, question):
     response = collection.query(
@@ -111,24 +112,6 @@ def get_nearest_chunk_from_chroma_vectordb(collection, question):
         n_results=1
     )
     return response['documents'][0][0], response['metadatas'][0][0]
-
-def generate(question, context, temperature, token_count):
-    mistral_sys = f"<s>[INST]You are a helpful, respectful and honest assistant. If you are unsure about an answer, truthfully say \"I don't know\"."
-    if context == "":
-        mistral_inst = f"Please answer the user question.[/INST]</s>"
-        question_and_context = f"{mistral_sys} {mistral_inst} \n [INST] {question} [/INST]"
-    else:
-        mistral_inst = f"Answer the user's question based on the following information:\n {context}[/INST]</s>"
-        question_and_context = f"{mistral_sys} {mistral_inst} \n[INST] {question} [/INST]"
-        
-    inputs = tokenizer(question_and_context, return_tensors="pt")
-    output = model.generate(
-        inputs["input_ids"],
-        max_length=token_count,
-        temperature=temperature,
-        do_sample=True
-    )
-    return tokenizer.decode(output[0], skip_special_tokens=True)
 
 if __name__ == "__main__":
     main()
